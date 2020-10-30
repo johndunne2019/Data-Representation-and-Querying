e8e53e8d7eb2da8e330a11624c7d692bc4517e2e
John Dunne <G00273895@gmit.ie>
2020-10-30 15:54:39 +0000

    Week 6 lecture code

---
+++ Week06/jsonPackage.py
@@ -0,0 +1,13 @@
+iimport json
+
+data =  {
+    'name':'joe',
+    'age':21,
+    "student": True
+    }
+#print(data)
+
+file = open("simple.json",'w')
+#json.dump(data,file, indent=4)
+jsonString = json.dumps(data)
+print(jsonString)
